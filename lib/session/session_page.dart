55e6f298f9711ab93e642e5fd8679fb7b4eb63fb
oguzcc <oguzcc@gmail.com>
2022-01-26 17:05:54 +0300

    sign out button added

--- lib/session/session_page.dart
+++ lib/session/session_page.dart
@@ -1,4 +1,7 @@
+import '../data/auth_repository.dart';
+import 'session_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../util/components/app_bar.dart';
 
 class SessionPage extends StatelessWidget {
@@ -17,9 +20,22 @@ class SessionView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(text: 'Session Page'),
-      body: const Center(
-        child: Text('Session View'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Session View'),
+            _signOutButton(context),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _signOutButton(BuildContext context) {
+    return TextButton(
+      onPressed: context.read<SessionCubit>().signOut,
+      child: const Text('Sign Out'),
     );
   }
 }
